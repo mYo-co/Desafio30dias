40bea029e227cb5b29134bc6b7ea2ee0ff2b2554
mYo <trunkstruly@gmail.com>
2026-09-20 21:57:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener("click", e => {
+    const target = document.querySelector(link.getAttribute("href"));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: "smooth", block: "start" });
+    }
+  });
+});
